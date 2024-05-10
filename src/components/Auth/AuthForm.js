6948289f1c2f260036a2bb83e4647de9f9665155
ranjandhal73 <ranjandhal.doc@gmail.com>
2024-05-10 20:57:51 +0530

    Implemented Login functionality

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from 'react';
-
+import { useState, useRef, useContext } from 'react';
 import classes from './AuthForm.module.css';
+import { AuthContext } from '../../store/auth-store';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+  const {login} = useContext(AuthContext)
+
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -16,12 +19,21 @@ const AuthForm = () => {
     e.preventDefault();
     const email = emailInputRef.current.value;
     const password = passwordInputRef.current.value;
-    const userData = {email,password}
 
-    setIsLoading(true)
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDt4C81D9NsgYpcCc2T6EZLMqsXfiQ0gO4',{
+    setIsLoading(true);
+    let url;
+    if(isLogin){
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDt4C81D9NsgYpcCc2T6EZLMqsXfiQ0gO4'
+    }else{
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDt4C81D9NsgYpcCc2T6EZLMqsXfiQ0gO4'
+    }
+    fetch(url,{
       method: 'POST',
-      body: JSON.stringify({userData,returnSecureToken:true}),
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken:true
+      }),
       headers: {
         'Content-type': 'application/json',
       }
@@ -29,15 +41,20 @@ const AuthForm = () => {
     .then((res) => {
       setIsLoading(false)
       if(res.ok){
-        
+        return res.json();
       }else{
-        return res.json().then(data => alert(JSON.stringify(data)))
+        return res.json()
+        .then(data => {
+          throw new Error(JSON.stringify(data.error.message))
+        })
       }
+    }).then((data)=>{
+      alert("Login Successfully")
+      login(data.idToken)
+    }).catch((err) =>{
+      alert(err.message)
     })
-    .catch((err) =>{
-      // setIsLoading(true);
-      console.error('Error:',err)
-    })
+  
     emailInputRef.current.value = '';
     passwordInputRef.current.value = '';
   }
